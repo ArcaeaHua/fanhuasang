6fddea423f783ebea52db6dd94310d237f5c7a4a
qiuxiaoluo <11503761+qiuxiaoluozero@user.noreply.gitee.com>
2023-06-20 23:48:56 +0800

    项目初始化超级

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    App
+    <p>{{ $store.state.username }}</p>
+  </div>
+</template>
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    const store = useStore()
+    console.log(store.state.username)
+  }
+}
+</script>
